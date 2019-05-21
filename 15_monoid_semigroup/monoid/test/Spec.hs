7ace807d9e2166ea538ef2d8a8e4d1fd6131c47d
Jonathan Lorimer <jonathan_lorimer@mac.com>
2019-05-20 21:20:16 -0400

    got quickcheck tests working

--- 15_monoid_semigroup/monoid/test/Spec.hs
+++ 15_monoid_semigroup/monoid/test/Spec.hs
@@ -1,16 +1,24 @@
 module Main where
 
-import Test.QuickCheck
+import           Lib
+import           Test.QuickCheck
 
 firstMappend :: First a -> First a -> First a
 firstMappend = mappend
 
-type FirstMappend = First String
-                  -> First String
-                  -> First String
-                  -> Bool
+type FirstMappend = First String -> First String -> First String -> Bool
 
 type FstId = First String -> Bool
+
+
+monoidAssoc :: Eq a => Monoid a => a -> a -> a -> Bool
+monoidAssoc x y z = (x <> y) <> z == x <> (y <> z)
+
+monoidLeftIdentity :: Eq a => Monoid a => a -> Bool
+monoidLeftIdentity x = mempty <> x == x
+
+monoidRightIdentity :: Eq a => Monoid a => a -> Bool
+monoidRightIdentity x = x <> mempty == x
 
 main :: IO ()
 main = do

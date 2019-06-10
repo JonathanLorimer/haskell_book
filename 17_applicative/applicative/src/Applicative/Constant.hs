6cc609418d491d8a9e72f7c2ef4d4506d07c4e1f
Jonathan Lorimer <jonathan_lorimer@mac.com>
2019-06-10 11:39:05 -0400

    added checkers to package.yaml

--- 17_applicative/applicative/src/Applicative/Constant.hs
+++ 17_applicative/applicative/src/Applicative/Constant.hs
@@ -4,10 +4,9 @@ newtype Constant a b =
     Constant { getConstant :: a }
         deriving (Eq, Ord, Show)
 
-instance Functor (Constant a)
-    where
-    fmap = undefined
+instance Functor (Constant a) where
+    fmap f (Constant a) = Constant a
 
 instance Monoid a => Applicative (Constant a) where
-    pure  = undefined
-    (<*>) = undefined
+    pure _ = Constant mempty
+    (<*>) (Constant x) (Constant x') = Constant (mappend x x')

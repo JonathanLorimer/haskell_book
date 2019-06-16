8d2ceff74bde08f783547e13b57d9e4283c12cf6
Jonathan Lorimer <jonathan_lorimer@mac.com>
2019-06-16 15:29:52 -0400

    finished applicative

--- 17_applicative/applicative/src/Applicative/Instances.hs
+++ 17_applicative/applicative/src/Applicative/Instances.hs
@@ -33,8 +33,29 @@ instance (Monoid a, Monoid b) => Applicative (Three a b) where
 -- | 4.
 data Three' a b = Three' a b b
 
+instance Functor (Three' a) where
+    fmap f (Three' a b b') = Three' a (f b) (f b')
+
+instance Monoid a => Applicative (Three' a) where
+    pure b = Three' mempty b b
+    (<*>) (Three' x f f') (Three' y a a') = Three' (x `mappend` y) (f a) (f' a')
+
 -- | 5.
 data Four a b c d = Four a b c d
 
+instance Functor (Four a b c) where
+    fmap f (Four a b c d) = Four a b c (f d)
+
+instance (Monoid a, Monoid b, Monoid c) => Applicative (Four a b c) where
+    pure d = Four mempty mempty mempty d
+    (<*>) (Four x x' x'' f) (Four y y' y'' d) = Four (x `mappend` y) (x' `mappend` y') (x'' `mappend` y'') (f d)
+
 -- | 6.
 data Four' a b = Four' a a a b
+
+instance Functor (Four' a) where
+    fmap f (Four' a a' a'' b) = Four' a a' a'' (f b)
+
+instance (Monoid a) => Applicative (Four' a) where
+    pure b = Four' mempty mempty mempty b
+    (<*>) (Four' x x' x'' f) (Four' y y' y'' b) = Four' (x `mappend` y) (x' `mappend` y') (x'' `mappend` y'') (f b)

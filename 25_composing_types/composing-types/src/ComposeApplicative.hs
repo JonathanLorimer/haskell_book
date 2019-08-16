5438f664b8aaa9c323e5e168181f5aa2fef1a8e4
Jonathan Lorimer <jonathan_lorimer@mac.com>
2019-08-16 09:37:14 -0400

    compose applicative instance

---
+++ 25_composing_types/composing-types/src/ComposeApplicative.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module ComposeApplicative where
+
+newtype Compose f g a =
+  Compose
+    { getCompose :: f (g a)
+    }
+  deriving (Eq, Show)
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+  pure :: a -> Compose f g a
+  pure a = Compose $ (pure pure) <*> (pure a)
+  (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+  (Compose fgf) <*> (Compose fga) = Compose $ pure (<*>) <*> fgf <*> fga
